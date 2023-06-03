e696e09a3388b1b1a72a90a5f608abe9c5a005e7
me-cafebabe <me.cafebabe@gmail.com>
2023-06-03 18:02:45 +0800

    Add Xiaomi uter

---
+++ config/xiaomi-uter.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[gx_rm67198_amoled_1080p_video]="xiaomi,uter-gx_rm67198_amoled"
+	[hh_rm67191_amoled_1080p_video]="xiaomi,uter-hh_rm67191_amoled"
+	[sharp_ft8716_fhd_video]="xiaomi,uter-sharp_ft8716"
+	[shenchao_ft8716_fhd_video]="xiaomi,uter-shenchao_ft8716"
+	[td4310_tianma_fhd_video]="xiaomi,uter-td4310_tianma"
+)
