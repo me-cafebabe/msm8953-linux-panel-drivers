b2d00d9be6e3aacae7abfc87c2f8c995f67c3e7d
me-cafebabe <me.cafebabe@gmail.com>
2023-06-04 02:15:29 +0800

    [TEMP] keep only uter

--- config/xiaomi-markw.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-OPTIONS=(-r vsn -r vsp)
-PANELS=(
-	[r63350_1080p_video]="mdss,r63350"
-	[nt35596_ebbg_1080p_video]="mdss,nt35596-ebbg"
-)
--- config/xiaomi-oxygen.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-OPTIONS=(-r vsn -r vsp)
-PANELS=(
-	[auo_r61322_1080p_video]="xiaomi,auo-r61322"
-	[tianma_r63350_1080p_video]="xiaomi,tianma-r63350"
-)
